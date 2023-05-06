73c3c6f31f12f6db2bf934b68d91b98f85515c1b
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-06 18:33:00 +0200

    Navigation list

--- src/components/+burger.svelte
+++ src/components/+burger.svelte
@@ -1,14 +1,35 @@
 <script>
     import { theNavigation } from "../store/store";
+    import { Motion } from "svelte-motion";
+
     const toggleTheNavigation = () => {
         theNavigation.update((value) => !value);
     };
 
-    console.log(theNavigation);
+    let navigationState = false;
+    theNavigation.subscribe(value => {
+        console.log("theNavigation value:", value);
+        navigationState = value;
+    });
 </script>
 
-<div class="flex flex-col gap-1" on:click={toggleTheNavigation}>
-    <div class="w-5 h-[2px] bg-white rounded-lg" />
-    <div class="w-5 h-[2px] bg-white rounded-lg" />
-    <div class="w-5 h-[2px] bg-white rounded-lg" />
+
+<div class="flex flex-col gap-1" on:click={toggleTheNavigation} on:keydown={(e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      toggleTheNavigation();
+    }
+  }}>
+    <Motion animate={{rotate: navigationState ? -45 : 0,
+                    y: navigationState ? 1.3 : 0
+                    }} let:motion>
+        <div use:motion class="w-5 h-[2px] bg-white rounded-lg" />
+    </Motion>
+    <Motion animate={{opacity: navigationState ? 0 : 1}} let:motion>
+        <div use:motion class="w-5 h-[2px] bg-white rounded-lg" />
+    </Motion>
+    <Motion animate={{rotate: navigationState ? 45 : 0 ,
+                    y: navigationState ? -10.5 : 0
+                    }} let:motion>
+        <div use:motion class="w-5 h-[2px] bg-white rounded-lg" />
+    </Motion>
 </div>
--- src/components/+navigation.svelte
+++ src/components/+navigation.svelte
@@ -1,14 +1,28 @@
 <script>
     import { theNavigation } from "../store/store";
+    import { Motion } from "svelte-motion";
+
     let navigationState = false;
     theNavigation.subscribe(value => {
         console.log("theNavigation value:", value);
         navigationState = value;
     });
+
 </script>
 
 {#if navigationState}
-    <div 
-        class="w-screen h-full bg-[#3a3a3a] absolute top-10 left-0">
-    </div>
+    <Motion
+    initial={{opacity:0}}
+    animate={{opacity:1}}
+    transition={{duration:1}}
+    let:motion
+    >
+        <div use:motion class="w-screen h-full bg-[#3a3a3a] absolute top-10 left-0 flex flex-col justify-around items-center pb-28 text-white">
+            <a href="/">Home</a>
+            <a href="/bio">Bio</a>
+            <a href="/schedule">Schedule</a>
+            <a href="/gallery">Gallery</a>
+            <a href="/contacts">Contacts</a>
+        </div>
+    </Motion>
 {/if}
