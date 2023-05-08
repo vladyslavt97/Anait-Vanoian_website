fb89c7f25a60e4c41c4d80dca263c9afc14f21ee
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-08 19:26:42 +0200

    Schedule

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,15 +1,17 @@
-<div class="flex flex-col items-center gap-5 pb-20 font-sans">
-    <h1 class="mt-20 font-bold">Some Concerts</h1>
-    <h2>12.05.2023</h2>
-    <h3>New York City</h3>
-    <li>Bach Concerto for Two Violins</li>
-    <li>Louis Spohr - Duo I for Two Violins, Op. 67</li>
-    <li>Vivaldi - Concerto for Two Violins in A Minor</li>
-    <br/>
-    <h2>13.05.2023</h2>
-    <h3>New York City</h3>
-    <li>Bach Concerto for Two Violins</li>
-    <li>Louis Spohr - Duo I for Two Violins, Op. 67</li>
-    <li>Vivaldi - Concerto for Two Violins in A Minor</li>
+<div class="flex flex-col items-center gap-5 pb-20 font-sans min-h-screen">
+    <h1 class="mt-20 font-bold">Concerts</h1>
+    <div>
+        <h2 class="font-bold text-yellow-400">12.05.2023</h2>
+        <h3 class="italic">New York City</h3>
+        <li>Bach Concerto for Two Violins</li>
+        <li>Louis Spohr - Duo I for Two Violins, Op. 67</li>
+        <li>Vivaldi - Concerto for Two Violins in A Minor</li>
+        <br/>
+        <h2 class="font-bold text-yellow-400">13.05.2023</h2>
+        <h3 class="italic">New York City</h3>
+        <li>Bach Concerto for Two Violins</li>
+        <li>Louis Spohr - Duo I for Two Violins, Op. 67</li>
+        <li>Vivaldi - Concerto for Two Violins in A Minor</li>
+    </div>
 </div>
 
