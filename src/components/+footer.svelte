dbdacaad2fe6ceb8af850c15803729ad64f9a853
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-08 17:16:23 +0200

    bio and download

--- src/components/+footer.svelte
+++ src/components/+footer.svelte
@@ -1,3 +1,3 @@
-<div class=" bg-[#3a3a3a] text-white w-screen h-5 flex justify-center text-sm font-thin absolute bottom-0">
+<div class=" bg-[#3a3a3a] text-white w-screen h-5 flex justify-center text-sm font-thin">
     <h2>© 2023 Anait Vanoian • Violinist</h2>
 </div>
