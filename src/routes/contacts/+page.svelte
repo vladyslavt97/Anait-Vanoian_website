b5d296ecd7dd204183d3fc9487d924cdde60e3fc
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-14 10:15:28 +0200

    trying to fix public key issue

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -3,8 +3,6 @@
     import IoIosText from 'svelte-icons/io/IoIosText.svelte'
     import emailjs from "@emailjs/browser";
     
-    import { onMount } from 'svelte';
-
     let emailState = '';
     let messageState = '';
     let sending = false;
@@ -15,20 +13,17 @@
     sending = true;
     console.log('email', emailState);
     console.log(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID ,process.env.YOUR_PUBLIC_KEY);
-    // let form = document.getElementById('myForm')
     if (
       process.env.YOUR_SERVICE_ID &&
       process.env.YOUR_TEMPLATE_ID &&
       process.env.YOUR_PUBLIC_KEY
-      // form
     ) {
       emailjs
         .sendForm(
           process.env.YOUR_SERVICE_ID,
           process.env.YOUR_TEMPLATE_ID,
           '#myForm',
-          process.env.YOUR_PUBLIC_KEY
-        )
+          process.env.YOUR_PUBLIC_KEY)
         .then(
           (result) => {
             console.log('resultsss', result.text);
@@ -50,9 +45,6 @@
     
   };
 
-  onMount(() => {
-    emailjs.init(process.env.YOUR_USER_ID);
-  });
 </script>
 
 <div class="min-h-screen sm:min-h-[98.5vh]">
@@ -60,9 +52,9 @@
 
     <form class="flex flex-col justify-center h-[70vh] sm:h-[80vh] m-10 font-sans md:w-[400px] md:mx-auto" on:submit|preventDefault={sendEmail} id="myForm">
         <label for="Email" class="w-16 flex flex-row items-center gap-2 text-gray-500">Email<IoIosMail/></label>
-        <input type="text" class="rounded" placeholder="provide your email" bind:value={emailState}>
+          <input type="text" class="rounded" placeholder="provide your email" bind:value={emailState}>
         <label for="Message" class="mt-10 w-24 flex flex-row items-center gap-1 text-gray-500">Message <IoIosText/></label>
-        <textarea class="rounded h-40" placeholder="write your message here" bind:value={messageState}/>
+          <textarea class="rounded h-40" placeholder="write your message here" bind:value={messageState}/>
         <button class="bg-gray-300 rounded-lg mt-2" type="submit">{sending ? 'Sending...' : 'Send'}</button>
     </form>
 </div>
