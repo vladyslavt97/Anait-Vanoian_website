86955e00afa7817eb9bfca421f1d60df7bf7fd32
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-17 19:15:33 +0200

    News one added

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,3 +1,11 @@
-<div class="p-10 min-h-screen sm:min-h-[98.5vh] flex flex-col justify-center items-center">
-    <h1>Welcome to News</h1>
+<div
+    class="p-10 min-h-screen sm:min-h-[98.5vh] flex flex-col justify-center items-center"
+>
+    <h1 class="absolute top-20">Welcome to News</h1>
+    <h2 class="font-mono">Some news info...</h2>
+    <a
+        class=" font-sans text-blue-900 underline"
+        href="https://www.klassikauseberswalde.de/wp-content/uploads/2022/05/steckbrief_anait_vanoian.pdf"
+        >Some link</a
+    >
 </div>
