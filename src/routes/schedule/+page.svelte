7c88b88fa440f0a80fb47abfa0ac73ad5531c796
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-17 19:12:06 +0200

    schedule styiling

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,39 +1,39 @@
 <script>
-    import { SyncLoader } from 'svelte-loading-spinners';
+    import { SyncLoader } from "svelte-loading-spinners";
     import { onMount } from "svelte";
 
-    let theConcerts = [{date: "encrypted", location: "", programme: ""}];
-    onMount(async()=>{
+    let theConcerts = [{ date: "encrypted", location: "", programme: "" }];
+    onMount(async () => {
         try {
-        const response = await fetch('/api/getconcerts');
+            const response = await fetch("/api/getconcerts");
             if (response.ok) {
                 theConcerts = await response.json();
             } else {
-                throw new Error('Failed to fetch data');
+                throw new Error("Failed to fetch data");
             }
         } catch (error) {
-        console.error('error', error);
+            console.error("error", error);
         }
-    })
-    
+    });
 </script>
 
-<div class="flex flex-col justify-center absolute left-16 gap-5 pb-20 font-sans min-h-screen sm:min-h-[98.5vh]">
-    <h1 class="mt-20 font-bold text-center absolute top-0">Schedule</h1>
-    
+<div
+    class="flex flex-col justify-start gap-5 pb-20 font-sans min-h-screen sm:min-h-[98.5vh]"
+>
+    <h1 class="mt-20 font-bold text-center">Schedule</h1>
+
     {#if theConcerts[0].date === "encrypted"}
-        <p><SyncLoader color="#ff9500"/></p>
+        <p><SyncLoader color="#ff9500" /></p>
     {:else}
         {#each theConcerts as concert}
-            <div>
+            <div class="mx-auto">
                 <h2 class="font-bold text-yellow-400">{concert.date}</h2>
                 <h3 class="italic">{concert.location}</h3>
                 {#each concert.programme as programItem}
                     <li>{programItem}</li>
                 {/each}
-                <br/>
+                <br />
             </div>
         {/each}
     {/if}
 </div>
-
