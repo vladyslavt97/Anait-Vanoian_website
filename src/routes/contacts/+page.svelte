7c13265dc1fff495ea663a5faaa7741df25baf4d
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-19 20:33:48 +0200

    subject and notes in readme

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,30 +1,71 @@
 <script>
-    import IoIosMail from 'svelte-icons/io/IoIosMail.svelte'
-    import IoIosText from 'svelte-icons/io/IoIosText.svelte'
-    
-    let emailState = '';
-    let messageState = '';
+    import IoIosMail from "svelte-icons/io/IoIosMail.svelte";
+    import IoIosText from "svelte-icons/io/IoIosText.svelte";
 
-  const sendEmail = () => {
-    let mailTo = `mailto:anaitvanoian@gmail.com?subject=Email From Your Website&body=${messageState} (${emailState})`;
-    window.location.href = mailTo;
-  }
+    let emailState = "";
+    let messageState = "";
+    let messageSubject = "";
 
+    const sendEmail = () => {
+        let mailTo = `mailto:anaitvanoian@gmail.com?subject=${messageSubject}&body=${messageState} (${emailState})`;
+        window.location.href = mailTo;
+    };
 
-  const clearValues = () => {
-    emailState = "";
-    messageState = "";
-  }
+    const clearValues = () => {
+        emailState = "";
+        messageState = "";
+        messageSubject = "";
+    };
 </script>
 
 <div class="min-h-screen sm:min-h-[98.5vh]">
     <h1 class="pt-20 text-center font-sans font-bold">Welcome to Contacts</h1>
 
-    <form class="flex flex-col justify-center h-[70vh] sm:h-[80vh] m-10 font-sans md:w-[400px] md:mx-auto" on:submit|preventDefault={sendEmail} id="myForm">
-        <label for="Email" class="w-16 flex flex-row items-center gap-2 text-gray-500">Email<IoIosMail/></label>
-          <input type="text" class="rounded" placeholder="provide your email" name="user_name" bind:value={emailState}>
-        <label for="Message" class="mt-10 w-24 flex flex-row items-center gap-1 text-gray-500">Message <IoIosText/></label>
-          <textarea class="rounded h-40" placeholder="write your message here" name="user_email" bind:value={messageState}/>
-        <button class="bg-gray-300 rounded-lg mt-2" type="submit" value="Send" on:click={clearValues}>Send</button>
+    <form
+        class="flex flex-col justify-center h-[70vh] sm:h-[80vh] m-10 font-sans md:w-[400px] md:mx-auto"
+        on:submit|preventDefault={sendEmail}
+        id="myForm"
+    >
+        <label
+            for="Email"
+            class="w-16 flex flex-row items-center gap-2 text-gray-500"
+            >Email<IoIosMail /></label
+        >
+        <input
+            type="text"
+            class="rounded"
+            placeholder="provide your email"
+            name="user_name"
+            bind:value={emailState}
+        />
+        <label
+            for="Subject"
+            class="w-16 flex flex-row items-center gap-2 text-gray-500"
+            >Subject</label
+        >
+        <input
+            type="text"
+            class="rounded"
+            placeholder="enter subject"
+            name="subject"
+            bind:value={messageSubject}
+        />
+        <label
+            for="Message"
+            class="mt-10 w-24 flex flex-row items-center gap-1 text-gray-500"
+            >Message <IoIosText /></label
+        >
+        <textarea
+            class="rounded h-40"
+            placeholder="write your message here"
+            name="user_email"
+            bind:value={messageState}
+        />
+        <button
+            class="bg-gray-300 rounded-lg mt-2"
+            type="submit"
+            value="Send"
+            on:click={clearValues}>Send</button
+        >
     </form>
 </div>
