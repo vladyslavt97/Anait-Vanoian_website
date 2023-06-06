1b0b96a0a157baafa1c8666e5ec305b5abd93086
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-06-06 17:09:16 +0200

    news page

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,18 +1,7 @@
-<div
-    class="p-10 min-h-screen sm:min-h-[98.5vh] flex flex-col justify-center items-center"
->
-    <h1 class="absolute top-20">Welcome to News...</h1>
-    <div class="absolute top-32">
-        <h2 class="font-mono">Some news info...</h2>
-        <a
-            class=" font-sans text-blue-900 underline"
-            href="https://www.klassikauseberswalde.de/wp-content/uploads/2022/05/steckbrief_anait_vanoian.pdf"
-            >Some link</a
-        >
-        <br />
-        <br />
-        <br />
-        <h2 class="font-mono">Подяка</h2>
-        <img src="/подяка.jpg" alt="подяка" class=" w-28" />
+<div class="p-10 min-h-screen sm:min-h-[98.5vh] flex flex-col items-center">
+    <h1 class="relative top-10">Welcome to News...</h1>
+    <div class="relative top-32">
+        <h2 class="font-mono text-center">Подяка</h2>
+        <img src="/подяка.jpg" alt="подяка" class="p-10" />
     </div>
 </div>
