4193705bff8b0a651ed9f5b1a649aa263b1e0049
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-06-15 13:48:35 +0200

    news updated

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,7 +1,20 @@
-<div class="p-10 min-h-screen sm:min-h-[98.5vh] flex flex-col items-center">
-    <h1 class="relative top-10">Welcome to News</h1>
-    <div class="relative top-32 rounded-2xl border drop-shadow-lg">
+<h1 class="relative top-12 text-center">Welcome to News</h1>
+<div
+    class="p-16 min-h-screen sm:min-h-[98.5vh] flex flex-col items-center gap-5"
+>
+    <div class="relative rounded-2xl border drop-shadow-lg">
         <h2 class="font-mono text-center">Подяка</h2>
         <img src="/подяка.jpg" alt="подяка" class="p-10" />
+    </div>
+
+    <div class="relative rounded-2xl border drop-shadow-lg">
+        <h2 class="font-mono text-center">Höcherbuch mit der Musik</h2>
+        <!-- svelte-ignore a11y-media-has-caption -->
+        <video src="/demo.mp4" controls class="p-10" />
+    </div>
+
+    <div class="relative rounded-2xl border drop-shadow-lg">
+        <h2 class="font-mono text-center">Certificate</h2>
+        <img src="/certificate.jpg" alt="подяка" class="p-10" />
     </div>
 </div>
