ed8196b31c0b89f1c477f17adf682a7e16784189
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-06-20 18:08:02 +0200

    news

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -9,11 +9,14 @@
 
     <div class="relative rounded-2xl border drop-shadow-lg">
         <h2 class="font-mono text-center">Höcherbuch mit der Musik</h2>
-        <img src="/mozart.jpg" alt="подяка" class="p-10" />
-        <a
-            href="dsf"
-            class="text-blue-600 italic underline text-center lowercase">link</a
-        >
+        <iframe
+            src="https://www.youtube.com/embed/QOlgSJZgGlw"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            title="video1"
+            class="p-10"
+        />
     </div>
 
     <div class="relative rounded-2xl border drop-shadow-lg">
