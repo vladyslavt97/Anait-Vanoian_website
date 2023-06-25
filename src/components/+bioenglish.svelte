9e4e188aa5de5c2302ab71215908094e5404f457
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-06-25 10:45:56 +0200

    Eng bio

--- src/components/+bioenglish.svelte
+++ src/components/+bioenglish.svelte
@@ -1,12 +1,13 @@
 <p class="px-5 font-sans pb-10 sm:mx-10 lg:mx-40 text-sm leading-6">
     <span class="italic font-bold">Anait Vanoian</span> was born in 1991 in
-    Izmail, Ukraine. At the age of 6 she took her first violin lessons.<br />
+    Izmail, Ukraine.<br />At the age of 6 she took her first violin lessons.<br
+    />
     From 1998 until 2005 she was a student of T. Spodinetz at the School of Art ins
     Kramatorsk, Ukraine.<br />
     From 2005 until 2008 she visited the special music school with professor S. Evdokimov
-    as a teacher. Afterwards, she visited the National University of Arts in Charkiv
-    from 2008 until 2012 where she graduated with a bachelor’s degree with distinction.<br
-    /><br />
+    as a teacher. <br /> Afterwards, she visited the National University of Arts
+    in Charkiv from 2008 until 2012 where she graduated with a bachelor’s degree
+    with distinction.<br /><br />
     After resettling to Germany in 2013, Anait visited the Brahms Academy of Music
     in Hamburg with Professor T. Mikaelyan as a teacher. <br /><br />
 
@@ -28,34 +29,38 @@
     College, Aachen, Germany for students at theCologne University of Music (2nd
     prize, 2017) <br />
     • XXIII Premio Internazionale di Musica Gaetano Zinetti (92 points out of 100,
-    2018) <br />
+    2018) <br /><br /><br />
 
     She also participated in several festivals:<br />
     • As a scholarship holder of the Richard Wagner Society of Charkiv, she participated
     at the Bayreuth Festival, Germany where she presented a contemporary composition
-    of Denys Bocharov. • From 8.-20.8.2014 she played at the Vilalte Music Festival
-    in Corsavy, France as first violin in several different groups, such as trios,
-    quartets, and sextets. Anait participated in a plethora of master courses of
-    leading violinists where she played with violinists such as Vladimir Astrachanzev,
-    Aleksey Koshvanets, Natalya Boyarskaya, Anatoly Bazhenov, Maxim Rusanov and Thomas
-    Brandis. Further courses included playing with Markus Placci and Burkhard Maiss.
-    She also visited a master course with the “Aizuri” quartet from the USA and the
-    first violin of the “Armida” quartet, Martin Funda, from Berlin. From August
-    2015 on, Anait is the first violin of the D.U.R. Quartet in Bremen, Germany.
-    Anait and her colleagues are given instruments by the famous Italian violin maker
-    Alessandro Cicillati through the company “Violin Assets”. From October 2019 on,
-    she began working as deputy concert master at the “Neue Philharmonie”, Berlin.
-    For her dedication, Anait received scholarships from the German Programme for
-    gifted students (Deutschlandstipendium) from 2014 until 2015. <br /><br />
+    of Denys Bocharov.<br />
+    • From 8.-20.8.2014 she played at the Vilalte Music Festival in Corsavy, France
+    as first violin in several different groups, such as trios, quartets, and sextets.
+    <br /><br />
+    Anait participated in a plethora of master courses of leading violinists where
+    she played with violinists such as Vladimir Astrachanzev, Aleksey Koshvanets,
+    Natalya Boyarskaya, Anatoly Bazhenov, Maxim Rusanov and Thomas Brandis. Further
+    courses included playing with Markus Placci and Burkhard Maiss. She also visited
+    a master course with the “Aizuri” quartet from the USA and the first violin of
+    the “Armida” quartet, Martin Funda, from Berlin. <br /><br />
 
-    Furthermore, she received a scholarship from the Richard Wagner Society of
-    Charkiv. Work experience: • Concert Master at the Municipal Orchestra of
-    Delmenhorst • Concert Master at the Chamber Philharmonic Emsland • Section
-    Leader at the Silk Road Symphony Orchestra, Berlin • I Violin Tutti at “Neue
-    Philharmonie”, Hamburg • I Violin Tutti at “Hansa Philharmonie”, Hamburg •
-    Deputy concert master at “Neue Philharmonie”, Berlin • I Violin Tutti
-    (temporary substitute) – Theater für Niedersachsen, Hildesheim • I Violin
-    Tutti (temporary substitute) – “Deutsches Filmorchester Babelsberg”, Berlin
-    • II Violin Tutti – Russian-German Music Academy under supervision of
+    From August 2015 on, Anait is the first violin of the D.U.R. Quartet in
+    Bremen, Germany. Anait and her colleagues are given instruments by the
+    famous Italian violin maker Alessandro Cicillati through the company “Violin
+    Assets”.<br /><br /> From October 2019 on, she began working as deputy
+    concert master at the “Neue Philharmonie”, Berlin. For her dedication, Anait
+    received scholarships from the German Programme for gifted students
+    (Deutschlandstipendium) from 2014 until 2015. Furthermore, she received a
+    scholarship from the Richard Wagner Society of Charkiv. <br /><br />
+
+    Work experience: <br />
+    • Concert Master at the Municipal Orchestra of Delmenhorst • Concert Master at
+    the Chamber Philharmonic Emsland • Section Leader at the Silk Road Symphony Orchestra,
+    Berlin • I Violin Tutti at “Neue Philharmonie”, Hamburg • I Violin Tutti at “Hansa
+    Philharmonie”, Hamburg • Deputy concert master at “Neue Philharmonie”, Berlin
+    • I Violin Tutti (temporary substitute) – Theater für Niedersachsen, Hildesheim
+    • I Violin Tutti (temporary substitute) – “Deutsches Filmorchester Babelsberg”,
+    Berlin • II Violin Tutti – Russian-German Music Academy under supervision of
     Maestro Valery Gergiev, Berlin
 </p>
