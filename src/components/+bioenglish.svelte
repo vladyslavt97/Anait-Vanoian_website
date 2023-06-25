810a2f9cf24954e34ec2506a4aba3e1324af1c03
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-06-25 10:36:46 +0200

    English bio

--- src/components/+bioenglish.svelte
+++ src/components/+bioenglish.svelte
@@ -18,101 +18,44 @@
     instructor.
     <br /><br />
 
-    Anait ist Preisträgerin mehrerer Wettbewerbe, u.a.der folgenden:<br />
-    • <span class=" italic">„Bluebird of Happiness“</span> in Simferopol,
-    Ukraine (3. Preis, 2002)<br />
-    • <span class=" italic">„Silver String Music Competition“</span> in
-    Kramatorsk (1. Preis, 2004)<br />
-    •
-    <span class=" italic"
-        >Internationaler Streichquartett Wettbewerb in Radom</span
-    >, Polen (3.Preis, 16.8.2011), zusammen mit dem Nocturnum-Quartett<br />
-    •
-    <span class=" italic"
-        >4. Brigitte-Kempen-Wettbewerb des Fördervereins der Musikhochschule in
-        Aachen</span
-    >
-    für Studierende der HfMT Köln. (2. Preis, 23.11.2017)<br />
-    •
-    <span class=" italic"
-        >XXIII Premio Internazionale di Musica Gaetano Zinetti</span
-    >
-    (92 Punkten von 100, 23.09.2018)<br />
-    •
-    <span class=" italic">Medici International Music Competition</span>, London
-    (3. Preis, 2021)<br /><br />
+    Anait won several prizes at International competitions, such as:<br />
+    • “Bluebird of Happiness” in Simferopol, Ukraine (3rd prize, 2002) <br />
+    • “Silver String Music Competition” in Kramatorsk, Ukraine (1st prize, 2004)
+    <br />
+    • International String Quartet Competition in Radom, Poland (3rd prize, 2011)
+    together with the Nocturnum-Quartet <br />
+    • 4th Brigitte Kempen Competition of the Sponsoring Association of the Music
+    College, Aachen, Germany for students at theCologne University of Music (2nd
+    prize, 2017) <br />
+    • XXIII Premio Internazionale di Musica Gaetano Zinetti (92 points out of 100,
+    2018) <br />
 
-    Sie nahm auch an zahlreichen Festivals teil:<br />
-    5 Jahre lang war sie Mitglied im Richard-Wagner-Verein in Charkiw. Als Stipendiatin
-    hat sie an den Richard-Wagner-Festspielen in Bayreuth teilgenommen, wo sie zusammen
-    mit dem Komponisten Denys Bocharov eine zeitgenössische Komposition von ihm präsentiert
-    hat. Vom 8.- 20.8.2014 spielte sie beim Vilalte Musik-Festival in Corsavy, Frankreich,
-    als erste Geigerin in unterschiedlichen Besetzungen, in Trios, Quartetts und
-    Sextetts.<br /><br />
+    She also participated in several festivals:<br />
+    • As a scholarship holder of the Richard Wagner Society of Charkiv, she participated
+    at the Bayreuth Festival, Germany where she presented a contemporary composition
+    of Denys Bocharov. • From 8.-20.8.2014 she played at the Vilalte Music Festival
+    in Corsavy, France as first violin in several different groups, such as trios,
+    quartets, and sextets. Anait participated in a plethora of master courses of
+    leading violinists where she played with violinists such as Vladimir Astrachanzev,
+    Aleksey Koshvanets, Natalya Boyarskaya, Anatoly Bazhenov, Maxim Rusanov and Thomas
+    Brandis. Further courses included playing with Markus Placci and Burkhard Maiss.
+    She also visited a master course with the “Aizuri” quartet from the USA and the
+    first violin of the “Armida” quartet, Martin Funda, from Berlin. From August
+    2015 on, Anait is the first violin of the D.U.R. Quartet in Bremen, Germany.
+    Anait and her colleagues are given instruments by the famous Italian violin maker
+    Alessandro Cicillati through the company “Violin Assets”. From October 2019 on,
+    she began working as deputy concert master at the “Neue Philharmonie”, Berlin.
+    For her dedication, Anait received scholarships from the German Programme for
+    gifted students (Deutschlandstipendium) from 2014 until 2015. <br /><br />
 
-    2014 und 2015 war sie Stipendiatin des Deutschland-Stipendiums. Sie nahm an
-    zahlreichen Meisterkursen teil und spielte dort mit führenden Violinisten
-    wie Wladimir Astrachanzew, Aleksej Koshvanets, Natalja Boyarskaya, Anatoly
-    Bazhenov, Maxim Rusanow und Thomas Brandis, später auch mit Markus Placci
-    und Burghard Maiß, außerdem beim Masterkurs mit dem „Aizuri Quartett“ aus
-    Amerika und mit dem ersten Violinisten, Martin Funda, des „Armida Quartetts“
-    Berlin. Ab August 2015 ist sie die erste Geigerin des D.U.R.Quartetts in
-    Bremen.<br /><br />
-
-    Anait und ihre Kollegen von des D.U.R. Quartett haben Instrumente von des
-    berühmten italienischen Geigenbauers Alessandro Ciciliati zu bekommen, der
-    Sprechpartner ist „Violin Assets“.<br /><br />
-
-    Ab Oktober 2019 fängte sie als Stellvertreterin Konzertmasterin bei „Neue
-    Philharmonie“ Berlin an.<br />
-    Berufserfahrung:<br />
-    • Konzertmeisterin bei Städtisches Orchester Delmenhorst<br />
-    • Konzertmeisterin bei Kammerphilharmonie Emsland<br />
-    • Stimmführerin bei Silk Road Symphony Orchester in Berlin<br />
-    • I Violine Tutti bei Neue Philharmonie Hamburg<br />
-    • I Violine Tutti Hansa Philharmonie Hamburg<br />
-    • Stellvertreterin Konzertmeisterin bei Neue Philharmonie Berlin<br />
-    • I Violine Tutti (Aushilfe) – Niedersachsen Theater in Hildesheim<br />
-    • I Violine Tutti (Aushilfe) – Deutschen Filmorchester Babelsberg Berlin<br
-    />
-    • II Violine Tutti - Russisch-Deutschen MusikAkademie Berlin unter der Leitung
-    Maestro Valery Gergiev<br />
-    • Stellvertreterin Konzertmeisterin bei Brandenburgisches Konzertorchester Eberswalde
-    (Zeitvertrag)<br />
-    • Stellvertreterin Konzertmeisterin (Aushilfe) bei Staatstheater Cottbus
+    Furthermore, she received a scholarship from the Richard Wagner Society of
+    Charkiv. Work experience: • Concert Master at the Municipal Orchestra of
+    Delmenhorst • Concert Master at the Chamber Philharmonic Emsland • Section
+    Leader at the Silk Road Symphony Orchestra, Berlin • I Violin Tutti at “Neue
+    Philharmonie”, Hamburg • I Violin Tutti at “Hansa Philharmonie”, Hamburg •
+    Deputy concert master at “Neue Philharmonie”, Berlin • I Violin Tutti
+    (temporary substitute) – Theater für Niedersachsen, Hildesheim • I Violin
+    Tutti (temporary substitute) – “Deutsches Filmorchester Babelsberg”, Berlin
+    • II Violin Tutti – Russian-German Music Academy under supervision of
+    Maestro Valery Gergiev, Berlin
 </p>
-
-Anait won several prizes at International competitions, such as: • “Bluebird of
-Happiness” in Simferopol, Ukraine (3rd prize, 2002) • “Silver String Music
-Competition” in Kramatorsk, Ukraine (1st prize, 2004) • International String
-Quartet Competition in Radom, Poland (3rd prize, 2011) together with the
-Nocturnum-Quartet • 4th Brigitte Kempen Competition of the Sponsoring
-Association of the Music College, Aachen, Germany for students at the Cologne
-University of Music (2nd prize, 2017) • XXIII Premio Internazionale di Musica
-Gaetano Zinetti (92 points out of 100, 2018) She also participated in several
-festivals: • As a scholarship holder of the Richard Wagner Society of Charkiv,
-she participated at the Bayreuth Festival, Germany where she presented a
-contemporary composition of Denys Bocharov. • From 8.-20.8.2014 she played at
-the Vilalte Music Festival in Corsavy, France as first violin in several
-different groups, such as trios, quartets, and sextets. Anait participated in a
-plethora of master courses of leading violinists where she played with
-violinists such as Vladimir Astrachanzev, Aleksey Koshvanets, Natalya
-Boyarskaya, Anatoly Bazhenov, Maxim Rusanov and Thomas Brandis. Further courses
-included playing with Markus Placci and Burkhard Maiss. She also visited a
-master course with the “Aizuri” quartet from the USA and the first violin of the
-“Armida” quartet, Martin Funda, from Berlin. From August 2015 on, Anait is the
-first violin of the D.U.R. Quartet in Bremen, Germany. Anait and her colleagues
-are given instruments by the famous Italian violin maker Alessandro Cicillati
-through the company “Violin Assets”. From October 2019 on, she began working as
-deputy concert master at the “Neue Philharmonie”, Berlin. For her dedication,
-Anait received scholarships from the German Programme for gifted students
-(Deutschlandstipendium) from 2014 until 2015. Furthermore, she received a
-scholarship from the Richard Wagner Society of Charkiv. Work experience: •
-Concert Master at the Municipal Orchestra of Delmenhorst • Concert Master at the
-Chamber Philharmonic Emsland • Section Leader at the Silk Road Symphony
-Orchestra, Berlin • I Violin Tutti at “Neue Philharmonie”, Hamburg • I Violin
-Tutti at “Hansa Philharmonie”, Hamburg • Deputy concert master at “Neue
-Philharmonie”, Berlin • I Violin Tutti (temporary substitute) – Theater für
-Niedersachsen, Hildesheim • I Violin Tutti (temporary substitute) – “Deutsches
-Filmorchester Babelsberg”, Berlin • II Violin Tutti – Russian-German Music
-Academy under supervision of Maestro Valery Gergiev, Berlin
