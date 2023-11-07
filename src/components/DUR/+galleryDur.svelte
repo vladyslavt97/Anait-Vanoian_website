501fb3fd9c8e357c98e40c169635d5fe6a679010
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-11-07 22:42:33 +0100

    new photo

--- src/components/DUR/+galleryDur.svelte
+++ src/components/DUR/+galleryDur.svelte
@@ -1,8 +1,9 @@
 <script>
     import { Carousel } from "flowbite-svelte";
     const images = [
-        { id: 0, attribution: "anait", imgurl: "./quartetGallery/1.jpeg" },
+        { id: 0, attribution: "anait", imgurl: "./quartet/durbg.png" },
         { id: 2, attribution: "anait", imgurl: "./quartetGallery/3.jpeg" },
+        { id: 2, attribution: "anait", imgurl: "./quartetGallery/2.jpeg" },
         { id: 2, attribution: "anait", imgurl: "./quartetGallery/2.jpeg" },
     ];
     let showThumbs = false;
