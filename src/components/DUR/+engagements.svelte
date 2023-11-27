f7e1438d0ca82c473c0c94d134d59ef8e24badc1
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-11-27 11:45:43 +0100

    slight correction to the ucoming event

--- src/components/DUR/+engagements.svelte
+++ src/components/DUR/+engagements.svelte
@@ -6,15 +6,15 @@
 <h2 class="text-center font-serif font-medium text-xl">Engagements</h2>
 <div class="md:flex">
     <div class="w-full">
-        <!-- concert3 -->
-        <div class="shadow-lg rounded border-2 border-yellow-500/20 p-2 m-2 flex flex-col gap-2 items-start">
+        <!-- concert2 -->
+        <div class="shadow-lg rounded border-2 border-yellow-500/20 p-2 m-2 flex flex-col md:flex-row justify-center gap-2 items-start">
             <div>
                 <h1>3.12.2023 um 18.00</h1>
                 <a
                     href="https://www.feuerlein-geigenakademie.de/"
                     target="_blank"
-                    class="text-blue-500 italic"
-                    >FEUERLEIN GEIGENAKADEMIE
+                    class="text-blue-500 italic normal-case"
+                    >Feuerlein Geigenakademie
                 </a>
                 <div class="flex gap-2">
                     <h2 class="italic">Künstlerhof Alt - Lietzov 12,10587 Berlin</h2>
@@ -52,7 +52,7 @@
                 <img
                     src="/poster3.12.jpg"
                     alt="poster 3.12.2023"
-                    class="rounded-lg mx-auto "
+                    class="rounded-lg"
                     width="200"
                 />
             </div>
@@ -60,38 +60,6 @@
         </div>
     </div>
 
-    <!-- concert2 -->
-    <!-- <div class="w-full">
-        <div class="shadow-lg rounded border-2 border-yellow-500/20 p-2 m-2">
-            <h1>30.11.2023 um 19:00</h1>
-            <div class="text-xs">
-                <p class="pb-2">
-                    F.Mendelssohn – String Quartet in A minor,Op.13 - 1. Satz
-                </p>
-                <p class="pb-2">Ph.Glass – String Quartet Nr.5 - 4. Satz</p>
-                <p class="pb-2">
-                    D. Klebanov - String Quartet Nr.4 - 1. Satz - ukrainischer
-                    Komponist, die berühmte Thema Щедрик - Folks Weihnachtslied
-                </p>
-                <p class="pb-2">
-                    G.F.Haydn – String Quartet 'Sunrise' B Major - 1. Satz
-                </p>
-                <p class="pb-2">
-                    A.Borodin – String Quartet Nr. 2 in D Major - 1. Satz
-                </p>
-                <p class="pb-2">
-                    E.Grieg – String Quartet in G minor, Op.27 - 1. Satz
-                </p>
-                <p class="pb-2">
-                    A.Dvorak – String Quartet Nr.12, Op.96 'American Quartet' - 2.
-                    und 3. Sätzen
-                </p>
-            </div>
-
-            <br />
-            Spende ab 10€
-        </div>
-    </div> -->
     <!-- concert1 -->
     <div class="w-full">
         <div class="shadow-lg rounded border-2 border-yellow-500/20 p-2 m-2">
