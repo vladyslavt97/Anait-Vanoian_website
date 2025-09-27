ae2bdf0bc978882eae519552bc80f01395c7b0e4
Tsurkanenko Vladyslav <VTsurkanenko@stroeer.de>
2025-09-27 21:33:29 +0200

    German Bio updated

--- src/components/+biodeutsch.svelte
+++ src/components/+biodeutsch.svelte
@@ -1,83 +1,35 @@
 <p class="px-5 font-sans pb-10 sm:mx-10 lg:mx-40 text-sm leading-6">
-    <span class="italic font-bold">Anait Vanoian</span> wurde 1991 in Izmail,
-    Ukraine, geboren. Mit 6 Jahren nahm sie den ersten Geigenunterricht.<br />
-    Von 1998 bis 2005 war sie Schülerin von T. Spodinetz an der Schule für Künste
-    in Kramatorsk, Ukraine.<br />
-    Von 2005 bis 2008 besuchte sie bei Professor S. Evdokimov die Spezialmusikschule
-    in Charkiv und von 2008 bis 2012 absolvierte sie an der Nationalen Universität
-    für Künste in Charkiw ein Bachelor-Studium, das sie mit Auszeichnung („rotes
-    Diplom“) abschloss.<br /><br />
+    <span class="italic font-bold">Anait Vanoian</span>wurde 1991 in Izmail (Ukraine) geboren. Mit sechs Jahren erhielt sie ihren ersten Geigenunterricht.<br />
+    Von 1998 bis 2005 war sie Schülerin von T. Spodinetz an der Schule für Künste in Kramatorsk. Anschließend studierte sie von 2005 bis 2008 an der Spezialmusikschule in Charkiw bei Professor S. Evdokimov. Ihr Bachelorstudium an der Nationalen Universität für Künste in Charkiw (2008–2012) schloss sie mit Auszeichnung („rotes Diplom“) ab.<br />
 
-    Nach der Übersiedlung nach Deutschland besuchte sie 2013 ein Jahr lang das
-    Brahms-Konservatorium in Hamburg bei Professor T. Mikaelyan.<br /><br />
+    Nach ihrer Übersiedlung nach Deutschland setzte sie ihre Ausbildung 2013 am Brahms-Konservatorium in Hamburg bei Professor T. Mikaelyan fort.<br /><br />
+    Von 2014 bis 2017 absolvierte sie ihr Masterstudium an der Hochschule für Künste in Bremen bei Professorin Katrin Scholz. Ab 2017 folgte ein weiterführendes Studium an der Hochschule für Musik, Theater und Medien Hannover bei Professor Oliver Wille (Kuss-Quartett).<br /><br />
+    Anait ist Preisträgerin zahlreicher Wettbewerbe, darunter:<br />
+    • <span class=" italic">„Bluebird of Happiness“</span>, Simferopol, Ukraine (3. Preis, 2002)<br />
+    • <span class=" italic">„Silver String Music Competition“</span>, Kramatorsk (1. Preis, 2004)<br />
+    • <span class=" italic">Internationaler Streichquartett-Wettbewerb</span>, Radom, Polen (3. Preis, 2011, mit dem Nocturnum-Quartett)<br />
+    • <span class=" italic">4. Brigitte-Kempen-Wettbewerb</span>, Aachen (2. Preis, 2017)<br />
+    • <span class=" italic">XXIII Premio Internazionale di Musica Gaetano Zinetti</span> (92/100 Punkte, 2018)<br />
+    • <span class=" italic">Medici International Music Competition</span>, London (3. Preis, 2021)<br /><br />
 
-    Von 2014 bis 2017 absolvierte sie als Studentin bei Professorin Katrin
-    Scholz an der Hochschule für Künste in Bremen ein Master – Studium.<br /><br
-    />
+    Sie wirkte bei zahlreichen Festivals mit. Über fünf Jahre war sie Mitglied des Richard-Wagner-Vereins in Charkiw und nahm als Stipendiatin an den Bayreuther Festspielen teil, wo sie gemeinsam mit dem Komponisten Denys Bocharov eine zeitgenössische Komposition präsentierte. Vom 8. bis 20. August 2014 war sie als erste Geigerin beim Vilalte Musik-Festival in Corsavy (Frankreich) aktiv und spielte in verschiedenen Besetzungen – vom Trio bis zum Sextett.<br /><br />
 
-    Ab 2017 wird sie die hMtMh (Hochschule für Musik, Theater und Medien) in
-    Hannover besuchen und bei Professor Oliver Wille (Kuss-Quartett) studieren.
-    <br /><br />
-    Anait ist Preisträgerin mehrerer Wettbewerbe, u.a.der folgenden:<br />
-    • <span class=" italic">„Bluebird of Happiness“</span> in Simferopol,
-    Ukraine (3. Preis, 2002)<br />
-    • <span class=" italic">„Silver String Music Competition“</span> in
-    Kramatorsk (1. Preis, 2004)<br />
-    •
-    <span class=" italic"
-        >Internationaler Streichquartett Wettbewerb in Radom</span
-    >, Polen (3.Preis, 16.8.2011), zusammen mit dem Nocturnum-Quartett<br />
-    •
-    <span class=" italic"
-        >4. Brigitte-Kempen-Wettbewerb des Fördervereins der Musikhochschule in
-        Aachen</span
-    >
-    für Studierende der HfMT Köln. (2. Preis, 23.11.2017)<br />
-    •
-    <span class=" italic"
-        >XXIII Premio Internazionale di Musica Gaetano Zinetti</span
-    >
-    (92 Punkten von 100, 23.09.2018)<br />
-    •
-    <span class=" italic">Medici International Music Competition</span>, London
-    (3. Preis, 2021)<br /><br />
+    Darüber hinaus konzertierte sie mit renommierten Dirigenten und Solisten, darunter Marcus Bosch, Tim Henty, Valery Gergiev, Bem Palmer, Denis Matsuev usw., und Auftritte führten sie in bedeutende Konzertsäle wie die Berliner Philharmonie, das Gewandhaus Leipzig, den Gasteig München, die Elbphilharmonie Hamburg, die Uber Arena Berlin, die Barclays Arena Hamburg sowie die Zenith Arena Paris.<br /><br />
+    Von 2014 bis 2015 war sie Stipendiatin des Deutschland-Stipendiums. Sie nahm an zahlreichen Meisterkursen teil, unter anderem bei Wladimir Astrachanzew, Aleksej Koshvanets, Natalja Boyarskaya, Anatoly Bazhenov, Maxim Rusanow und Thomas Brandis. Später arbeitete sie auch mit Markus Placci, Burghard Maiß, dem Aizuri Quartett (USA) sowie Martin Funda (Armida Quartett, Berlin). Seit 2015 ist sie Primaria des D.U.R.-Quartetts Bremen, das mit Instrumenten des italienischen Geigenbauers Alessandro Ciciliati (Violin Assets) ausgestattet ist.<br /><br />
 
-    Sie nahm auch an zahlreichen Festivals teil:<br />
-    5 Jahre lang war sie Mitglied im Richard-Wagner-Verein in Charkiw. Als Stipendiatin
-    hat sie an den Richard-Wagner-Festspielen in Bayreuth teilgenommen, wo sie zusammen
-    mit dem Komponisten Denys Bocharov eine zeitgenössische Komposition von ihm präsentiert
-    hat. Vom 8.- 20.8.2014 spielte sie beim Vilalte Musik-Festival in Corsavy, Frankreich,
-    als erste Geigerin in unterschiedlichen Besetzungen, in Trios, Quartetts und
-    Sextetts.<br /><br />
-
-    2014 und 2015 war sie Stipendiatin des Deutschland-Stipendiums. Sie nahm an
-    zahlreichen Meisterkursen teil und spielte dort mit führenden Violinisten
-    wie Wladimir Astrachanzew, Aleksej Koshvanets, Natalja Boyarskaya, Anatoly
-    Bazhenov, Maxim Rusanow und Thomas Brandis, später auch mit Markus Placci
-    und Burghard Maiß, außerdem beim Masterkurs mit dem „Aizuri Quartett“ aus
-    Amerika und mit dem ersten Violinisten, Martin Funda, des „Armida Quartetts“
-    Berlin. Ab August 2015 ist sie die erste Geigerin des D.U.R.Quartetts in
-    Bremen.<br /><br />
-
-    Anait und ihre Kollegen von des D.U.R. Quartett haben Instrumente von des
-    berühmten italienischen Geigenbauers Alessandro Ciciliati zu bekommen, der
-    Sprechpartner ist „Violin Assets“.<br /><br />
-
-    Ab Oktober 2019 fängte sie als Stellvertreterin Konzertmasterin bei „Neue
-    Philharmonie“ Berlin an.<br />
-    Berufserfahrung:<br />
-    • Konzertmeisterin bei Städtisches Orchester Delmenhorst<br />
-    • Konzertmeisterin bei Kammerphilharmonie Emsland<br />
-    • Stimmführerin bei Silk Road Symphony Orchester in Berlin<br />
-    • I Violine Tutti bei Neue Philharmonie Hamburg<br />
-    • I Violine Tutti Hansa Philharmonie Hamburg<br />
-    • Stellvertreterin Konzertmeisterin bei Neue Philharmonie Berlin<br />
-    • I Violine Tutti (Aushilfe) – Niedersachsen Theater in Hildesheim<br />
-    • I Violine Tutti (Aushilfe) – Deutschen Filmorchester Babelsberg Berlin<br
-    />
-    • II Violine Tutti - Russisch-Deutschen MusikAkademie Berlin unter der Leitung
-    Maestro Valery Gergiev<br />
-    • Stellvertreterin Konzertmeisterin bei Brandenburgisches Konzertorchester Eberswalde
-    (Zeitvertrag)<br />
-    • Stellvertreterin Konzertmeisterin (Aushilfe) bei Staatstheater Cottbus
+    Seit Oktober 2019 bis 2022 war Anait als stellvertretende Konzertmeisterin/Konzertmeisterin bei der „Neuen Philharmonie Berlin“ tätig.<br/>
+    Berufserfahrung (Auswahl):<br />
+    • Konzertmeisterin – Städtisches Orchester Delmenhorst<br />
+    • Konzertmeisterin – Kammerphilharmonie Emsland<br />
+    • Stimmführerin – Silk Road Symphony Orchestra, Berlin<br />
+    • I. Violine Tutti – Neue Philharmonie Hamburg<br />
+    • I. Violine Tutti – Hansa Philharmonie Hamburg<br />
+    • Stellv. Konzertmeisterin – Neue Philharmonie Berlin<br />
+    • I. Violine Tutti (Aushilfe) – Theater für Niedersachsen, Hildesheim<br />
+    • I. Violine Tutti (Aushilfe) – Deutsches Filmorchester Babelsberg, Berlin<br />
+    • II. Violine Tutti – Russisch-Deutsche Musikakademie Berlin unter Maestro Valery Gergiev<br />
+    • Stellv. Konzertmeisterin – Brandenburgisches Konzertorchester Eberswalde (Zeitvertrag)<br />
+    • Stellv. Konzertmeisterin (Aushilfe) – Staatstheater Cottbus<br />
+    • I. Violine Tutti (Aushilfe) - Volkstheater Rostock<br />
+    • I. Violine Tutti (Aushilfe) - Brandenburger Theater<br />
 </p>
